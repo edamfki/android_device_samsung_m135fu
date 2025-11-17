dcfc9cfb9ac1acc2887ac058d71774d0979cd918
edamfki <125481012+edamfki@users.noreply.github.com>
2025-11-17 21:56:37 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,4 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/m135fu
+LOCAL_PATH := device/samsung/m13
+
+# Product properties for M135FU (Indian variant)
+PRODUCT_MODEL := SM-M135FU
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := m13
+PRODUCT_NAME := m13xx
+
+# Optional: Build fingerprint for FU variant
+# You can adjust the Android version / build ID as needed
+PRODUCT_FINGERPRINT := samsung/m13xx/m13:12/SP1A.210812.016:M135FU:user/release-keys
